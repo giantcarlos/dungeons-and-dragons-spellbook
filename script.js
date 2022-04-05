f49e430cc37bad040674a59f498ddf4777530243
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-04-05 17:49:01 -0500

    added clickable links event listener

--- script.js
+++ script.js
@@ -3,17 +3,49 @@ window.addEventListener('DOMContentLoaded', () => {
 })
 
 function getSpells() {
-    const ul = document.getElementById('spells')
+    const ul = document.getElementById('spells-list')
     fetch('https://www.dnd5eapi.co/api/spells')
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        console.log(Object.entries(data));
         dataArray = Object.entries(data);
         secondArray = dataArray["1"];
         spellArray = secondArray["1"];
         spellArray.forEach(spell => {
-            ul.innerHTML += `<li>${spell.name}</li>`
+            ul.innerHTML += `
+            <li><a href="#" data-url="${spell.url}">${spell.name}</a></li>
+            `
        })
+       attachLinks()
      })
+}
+
+const attachLinks = () => {
+    const spells = document.querySelectorAll('a')
+    spells.forEach((spell) => {
+        spell.addEventListener('click', displaySpell)
+    })
+}
+
+const displaySpell = (event) => {
+    console.log(event.target.dataset.url)
+    const info = document.getElementById('info')
+    const ul = document.getElementById('spells-list')
+    ul.innerHTML = ''
+    fetch('https://www.dnd5eapi.co' + `${event.target.dataset.url}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        info.innerHTML = `
+        <h2>${data.name}</h2><br>
+        <p>Level: ${data.level}</p>
+        <p>School: ${data.school.index}</p>
+        <p>Casting Time: ${data.casting_time}</p>
+        <p>Range: ${data.range}</p>
+        <p>Component: ${data.components.join(", ")} (${data.material})</p>
+        <p>Duration: ${data.duration}</p>
+        <p>Classes: ${data.classes}</p>
+        <p>${data.desc.join(", ")}</p>
+        <p>${data.higher_level.join(", ")}
+        `
+    })
 }
