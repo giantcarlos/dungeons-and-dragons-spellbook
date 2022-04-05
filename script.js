52fc71a4e4a9172096f313e03faf7f843809952e
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-04-05 13:52:48 -0500

    added fetched data to ul

--- script.js
+++ script.js
@@ -7,6 +7,13 @@ function getSpells() {
     fetch('https://www.dnd5eapi.co/api/spells')
     .then(res => res.json())
     .then(data => {
-       console.log(data)
+        console.log(data);
+        console.log(Object.entries(data));
+        dataArray = Object.entries(data);
+        secondArray = dataArray["1"];
+        spellArray = secondArray["1"];
+        spellArray.forEach(spell => {
+            ul.innerHTML += `<li>${spell.name}</li>`
+       })
      })
 }
