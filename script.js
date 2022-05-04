74cd06bf99e78aa3dfb8dffcccf6a4e42000edaf
Gian Taino Carlos <giantainocarlos@gmail.com>
2022-05-04 11:40:13 -0500

    added search bar

--- script.js
+++ script.js
@@ -2,6 +2,10 @@ window.addEventListener('DOMContentLoaded', () => {
     getSpells()
     document.getElementById("home").addEventListener('click', getSpells) 
     document.getElementById("classes").addEventListener('change', selectClass)
+    document.querySelector("form").addEventListener('submit', (e) => {
+        e.preventDefault()
+        searchSpell(e.target.search.value.toLowerCase().trim().split(/\s+/).join('-'))
+    })
 })
 
 const ul = document.getElementById('spells-list')
@@ -51,6 +55,27 @@ const displaySpell = (event) => {
     console.log(event.target.dataset.url)
     ul.innerHTML = ''
     fetch('https://www.dnd5eapi.co' + `${event.target.dataset.url}`)
+    .then(res => res.json())
+    .then(data => {
+        console.log(data)
+        info.innerHTML = `
+        <h2>${data.name}</h2><br>
+        <p>Level: ${(data.level === 0) ? "Cantrip" : `${data.level}`}</p>
+        <p>School: ${data.school.name}</p>
+        <p>Casting Time: ${data.casting_time} ${(data.ritual === true) ? "(Ritual)" : ""}</p>
+        <p>Range: ${data.range}</p>
+        <p>Components: ${data.components.join(", ")} ${(data.material === undefined) ? "" : `(${data.material})`}</p>
+        <p>Duration: ${data.duration} ${(data.concentration === true) ? "(Concentration)" : ""}</p>
+        <p>Classes: ${(data.classes.map(classes => classes.name)).join(", ")}</p>
+        <p>${data.desc.join(", ")}</p>
+        <p>${data.higher_level.join(", ")}
+        `
+    })
+}
+
+const searchSpell = (searcher) => {
+    ul.innerHTML = ''
+    fetch('https://www.dnd5eapi.co/api/spells/' + `${searcher}`)
     .then(res => res.json())
     .then(data => {
         console.log(data)
